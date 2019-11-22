fb185578c6efbd0b8082f75beddcb56d417be9e4
Felippe Nalin Teracini <felippent@al.insper.edu.br>
2019-11-22 14:50:49 -0300

    Add files via upload

--- main_mongo.py
+++ main_mongo.py
@@ -22,47 +22,48 @@ class Task(BaseModel):
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/task")
-def read_tasks():
+async def read_tasks():
     tasks_list = {}
     tasks_list["Tasks"] = []
     for i in tasks.find():
         tasks_list["Tasks"].append(
-            {"id": i["_id"], "title": i["title"], "description": i["description"]})
+            {"id": str(i["_id"]), "title": i["title"], "description": i["description"]})
     return tasks_list
 
 
 @app.post("/task")
-def create_task(task: Task):
+async def create_task(task: Task):
     new_task = {"title": task.title,
                 "description": task.description}
     tasks.insert_one(new_task)
 
 
 @app.get("/task/{task_id}")
-def read_task(task_id: int):
-    found_task = tasks.find({"_id": ObjectId(task_id)})
-    return_task = {"id": found_task["_id"], "title": found_task["title"],
-                   "description": found_task["description"]}
-    return return_task
+async def read_task(task_id: int):
+    tasks_list = {}
+    tasks_list["Tasks"] = []
+    for i in tasks.find({"_id": ObjectId(task_id)}):
+        tasks_list["Tasks"].append(
+            {"id": i["_id"], "title": i["title"], "description": i["description"]})
+    return tasks_list
 
 
 @app.put("/task/{task_id}")
-def update_task(task_id: int, task: Task):
+async def update_task(task_id: int, task: Task):
     tasks.update_one({"_id": ObjectId(task_id)}, {"$set": {
                      "title": task.title, "description": task.description}})
-    return task
 
 
 @app.delete("/task/{task_id}")
-def delete_task(task_id: int):
+async def delete_task(task_id: int):
     tasks.remove({"_id": ObjectId(task_id)})
 
 
 @app.get("/healthcheck")
-def read_health():
+async def read_health():
     return
