b7d5ed292b170185d0bf01331659104797aeaf95
Felippe Nalin Teracini <felippent@al.insper.edu.br>
2019-11-22 13:35:10 -0300

    Add files via upload

--- main_mongo.py
+++ main_mongo.py
@@ -29,37 +29,37 @@ def read_root():
 def read_tasks():
     tasks_list = {}
     for i in tasks.find():
-        tasks_list[i['_id']] = {'title': i['title'],
-                                'description': i['description']}
+        tasks_list[i["_id"]] = {"title": i["title"],
+                                "description": i["description"]}
     return tasks_list
 
 
 @app.post("/task")
 def create_task(task: Task):
-    new_task = {'title': task.title,
-                'description': task.description}
+    new_task = {"title": task.title,
+                "description": task.description}
     x = tasks.insert_one(new_task)
     return x.inserted_id
 
 
 @app.get("/task/{task_id}")
 def read_task(task_id: int):
-    found_task = tasks.find({'_id': ObjectId(task_id)})
-    return_task = {'id': found_task['_id'], 'title': found_task['title'],
-                   'description': found_task['description']}
+    found_task = tasks.find({"_id": ObjectId(task_id)})
+    return_task = {"id": found_task['_id'], "title": found_task["title"],
+                   "description": found_task["description"]}
     return return_task
 
 
 @app.put("/task/{task_id}")
 def update_task(task_id: int, task: Task):
-    tasks.update_one({'_id': ObjectId(task_id)}, {'$set': {
-                     'title': task.title, 'description': task.description}})
+    tasks.update_one({"_id": ObjectId(task_id)}, {"$set": {
+                     "title": task.title, "description": task.description}})
     return task
 
 
 @app.delete("/task/{task_id}")
 def delete_task(task_id: int):
-    tasks.remove({'_id': ObjectId(task_id)})
+    tasks.remove({"_id": ObjectId(task_id)})
 
 
 @app.get("/healthcheck")
